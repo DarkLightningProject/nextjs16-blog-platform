23a38acf80a738fc6dcd4465ca11d8b527b9680e
DarkLightningProject <ripudamansinghmultani@gmail.com>
2026-09-24 18:14:34 +0530

    V4.5.Fix

--- convex/posts.ts
+++ convex/posts.ts
@@ -30,7 +30,7 @@ export const createPost = mutation({
 export const getPosts = query({
   args: {},
   handler: async (ctx) => {
-  const posts = await ctx.db.query('posts').order('desc').take(100);
+   const posts = await ctx.db.query('posts').order('desc').collect();
   return await Promise.all(posts.map(async (post) => {
    const resolvedImageUrl = post.imageStorageId !== undefined ? await ctx.storage.getUrl(post.imageStorageId) : null;
    return { ...post, imageUrl: resolvedImageUrl };
