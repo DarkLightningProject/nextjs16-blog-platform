29a5eb524e7c27c7140b1995ee72342cc31f27bd
DarkLightningProject <ripudamansinghmultani@gmail.com>
2026-09-24 16:59:28 +0530

    v2 fix

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -52,7 +52,7 @@ async function LoadBlogList(){
   "use cache";
   cacheLife("hours");
   cacheTag("blog");
-  const data = await fetchQuery(api.posts.getPosts)
+  const data = await fetchQuery(api.posts.getPosts, {})
   return(
 
 <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
